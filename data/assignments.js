a879a1048a9cf6d869d93895ac84587d3e5cd44e
Shriyans Sairy <69405403+srisairy2@users.noreply.github.com>
2023-10-04 13:07:36 -0400

    Initial commit

---
+++ data/assignments.js
@@ -0,0 +1,199 @@
+assignments = {
+  "COMPSCI 164-LEC-001": [
+    {
+      "title": "hw0",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-08-31T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3079267/submissions/188466880",
+      "submitted": true
+    },
+    {
+      "title": "Drill 1",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-08-31T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3092782/submissions/188472050",
+      "submitted": true
+    },
+    {
+      "title": "hw1",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-14T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3207450/submissions/192232186",
+      "submitted": true
+    },
+    {
+      "title": "Drill 2",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-07T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3208606/submissions/190301789",
+      "submitted": true
+    },
+    {
+      "title": "Drill 3",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-14T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3283256/submissions/192093313",
+      "submitted": true
+    },
+    {
+      "title": "hw2-written",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-21T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3327431/submissions/194704702",
+      "submitted": true
+    },
+    {
+      "title": "hw2-code",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-09-21T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3327433/submissions/194758420",
+      "submitted": true
+    },
+    {
+      "title": "hw3",
+      "course": "COMPSCI 164-LEC-001",
+      "dueDate": "2023-10-05T21:00:00",
+      "link": "https://www.gradescope.com/courses/572216/assignments/3373601/submissions/199151824",
+      "submitted": true
+    }
+  ],
+  "CS 180/280A": [
+    {
+      "title": "Project 1 - Colorizing the Prokudin-Gorskii Photo Collection",
+      "course": "CS 180/280A",
+      "dueDate": "2023-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3186346/submissions/189914303",
+      "submitted": true
+    },
+    {
+      "title": "Project 2 - Fun with Filters and Frequencies",
+      "course": "CS 180/280A",
+      "dueDate": "2023-09-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3186347/submissions/194814806",
+      "submitted": true
+    },
+    {
+      "title": "Project 3 - Face Morphing and Modelling a Photo Collection",
+      "course": "CS 180/280A",
+      "dueDate": "2023-10-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/581661/assignments/3186348/submissions/198352868",
+      "submitted": true
+    }
+  ],
+  "CS 189/289A": [
+    {
+      "title": "HW 1 Code",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262/assignments/3184862/submissions/191544416",
+      "submitted": true
+    },
+    {
+      "title": "HW 1 Written",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262/assignments/3192847/submissions/191597948",
+      "submitted": true
+    },
+    {
+      "title": "HW 2 Written",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262/assignments/3306538/submissions/195996186",
+      "submitted": true
+    },
+    {
+      "title": "HW 2 Code",
+      "course": "CS 189/289A",
+      "dueDate": "2023-09-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262/assignments/3306914/submissions/195996139",
+      "submitted": true
+    },
+    {
+      "title": "HW 3 Written",
+      "course": "CS 189/289A",
+      "dueDate": "2023-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262",
+      "submitted": false
+    },
+    {
+      "title": "HW 3 Code",
+      "course": "CS 189/289A",
+      "dueDate": "2023-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/575262",
+      "submitted": false
+    }
+  ],
+  "CS198-008": [
+    {
+      "title": "Lab 01",
+      "course": "CS198-008",
+      "dueDate": "2023-09-09T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3255167/submissions/190953989",
+      "submitted": true
+    },
+    {
+      "title": "Vitamin 1",
+      "course": "CS198-008",
+      "dueDate": "2023-09-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3264956/submissions/190653419",
+      "submitted": true
+    },
+    {
+      "title": "Lab 02",
+      "course": "CS198-008",
+      "dueDate": "2023-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3310726/submissions/192232735",
+      "submitted": true
+    },
+    {
+      "title": "Vitamin 2",
+      "course": "CS198-008",
+      "dueDate": "2023-09-16T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3316849/submissions/192554276",
+      "submitted": true
+    },
+    {
+      "title": "Vitamin 3",
+      "course": "CS198-008",
+      "dueDate": "2023-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3320400/submissions/194823309",
+      "submitted": true
+    },
+    {
+      "title": "Lab 03",
+      "course": "CS198-008",
+      "dueDate": "2023-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3321098/submissions/193825423",
+      "submitted": true
+    },
+    {
+      "title": "Lab 04",
+      "course": "CS198-008",
+      "dueDate": "2023-09-30T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3409265/submissions/197253624",
+      "submitted": true
+    },
+    {
+      "title": "Vitamin 4",
+      "course": "CS198-008",
+      "dueDate": "2023-10-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3419185/submissions/197248249",
+      "submitted": true
+    },
+    {
+      "title": "Lab 05",
+      "course": "CS198-008",
+      "dueDate": "2023-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3451449/submissions/new",
+      "submitted": false
+    },
+    {
+      "title": "Vitamin 5",
+      "course": "CS198-008",
+      "dueDate": "2023-10-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/572351/assignments/3463644/submissions/new",
+      "submitted": false
+    }
+  ]
+};

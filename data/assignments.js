713216910290cdd44f2715a88555f4c7caa19667
planit <planit@tanjeffreyz.github.io>
2023-10-04 19:38:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,230 @@
-assignments = {};
+assignments = {
+  "CMSC 351 - 0201": [
+    {
+      "title": "Homework 0",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3220877/submissions/190940127",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 1",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-06T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3228299/submissions/189568706",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 3",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-06T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3239220/submissions/190732016",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 2",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-08T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3282425/submissions/191294760",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 4",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-08T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3282526/submissions/191326430",
+      "submitted": true
+    },
+    {
+      "title": "NP 1",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3282862/submissions/192881186",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 5",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-11T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3299278/submissions/191900658",
+      "submitted": true
+    },
+    {
+      "title": "Homework 1",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3311054/submissions/194315979",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 6",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-13T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3316071/submissions/192672920",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 7",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-18T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3332712/submissions/193310771",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 8",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-20T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3354943/submissions/194911893",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 9",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-20T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3365231/submissions/194923723",
+      "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3372754/submissions/196646761",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 10",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-22T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3373806/submissions/195606670",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 11",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-26T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3391586/submissions/196758424",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 12",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-27T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3414239/submissions/197071958",
+      "submitted": true
+    },
+    {
+      "title": "NP 2",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-27T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3414465/submissions/197428844",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 13",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-09-29T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3424082/submissions/197860052",
+      "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-10-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3424648/submissions/198858245",
+      "submitted": true
+    },
+    {
+      "title": "Quiz 14",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-10-02T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3440316/submissions/198630853",
+      "submitted": true
+    },
+    {
+      "title": "Homework 4",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-10-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213",
+      "submitted": false
+    },
+    {
+      "title": "Quiz 15",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-10-04T14:00:00",
+      "link": "https://www.gradescope.com/courses/597213/assignments/3456750/submissions/199304658",
+      "submitted": true
+    }
+  ],
+  "CMSC330": [
+    {
+      "title": "Project 0",
+      "course": "CMSC330",
+      "dueDate": "2023-09-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3141000/submissions/190083500",
+      "submitted": true
+    },
+    {
+      "title": "Lecture Quiz 8/31",
+      "course": "CMSC330",
+      "dueDate": "2023-09-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3229332/submissions/190248809",
+      "submitted": true
+    },
+    {
+      "title": "Discussion 1",
+      "course": "CMSC330",
+      "dueDate": "2023-09-01T17:00:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3233444/submissions/189807473",
+      "submitted": true
+    },
+    {
+      "title": "Project 1",
+      "course": "CMSC330",
+      "dueDate": "2023-09-10T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3242355/submissions/191870461",
+      "submitted": true
+    },
+    {
+      "title": "Lecture Quiz 9/7",
+      "course": "CMSC330",
+      "dueDate": "2023-09-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3283318/submissions/192175781",
+      "submitted": true
+    },
+    {
+      "title": "Project 2",
+      "course": "CMSC330",
+      "dueDate": "2023-09-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3295642/submissions/194905233",
+      "submitted": true
+    },
+    {
+      "title": "Lecture Quiz 9/14",
+      "course": "CMSC330",
+      "dueDate": "2023-09-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3334172/submissions/194371049",
+      "submitted": true
+    },
+    {
+      "title": "Project 3",
+      "course": "CMSC330",
+      "dueDate": "2023-10-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3359767/submissions/199139959",
+      "submitted": true
+    },
+    {
+      "title": "Lecture Quiz 9/21",
+      "course": "CMSC330",
+      "dueDate": "2023-09-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034",
+      "submitted": false
+    },
+    {
+      "title": "Lecture Quiz 9/28 ",
+      "course": "CMSC330",
+      "dueDate": "2023-10-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3435602/submissions/198823945",
+      "submitted": true
+    }
+  ]
+};

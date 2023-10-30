8c9f1f4b80b834df4ac881ba77bb1a72a8f3b43e
planit <planit@tanjeffreyz.github.io>
2023-10-30 20:38:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -237,6 +237,13 @@ assignments = {
       "dueDate": "2023-11-01T23:59:00",
       "link": "https://www.gradescope.com/courses/597213",
       "submitted": false
+    },
+    {
+      "title": "NP 4 ",
+      "course": "CMSC 351 - 0201",
+      "dueDate": "2023-11-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/597213",
+      "submitted": false
     }
   ],
   "CMSC330": [

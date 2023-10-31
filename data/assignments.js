8a2eaa33320d7ba7ecb917ee7e1ee9ce6b7a6b4f
planit <planit@tanjeffreyz.github.io>
2023-10-31 03:38:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -356,8 +356,8 @@ assignments = {
       "title": "Lecture Quiz 10/26",
       "course": "CMSC330",
       "dueDate": "2023-10-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/561034/assignments/3591624/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/561034/assignments/3591624/submissions/207846888",
+      "submitted": true
     }
   ]
 };

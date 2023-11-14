41ef7a8dadeb1ea80b542e00a325f0c5b4459cd4
planit <planit@tanjeffreyz.github.io>
2023-11-14 04:38:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -412,7 +412,7 @@ assignments = {
       "title": "Project 6",
       "course": "CMSC330",
       "dueDate": "2023-11-15T23:59:00",
-      "link": "https://www.gradescope.com/courses/561034/assignments/3616438/submissions/211991654",
+      "link": "https://www.gradescope.com/courses/561034/assignments/3616438/submissions/212411682",
       "submitted": true
     },
     {

d296300aafc554bbad478951f8c3f3c8e80c44c4
planit <planit@tanjeffreyz.github.io>
2024-01-25 20:38:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
   "CMSC 426": [],
-  "STAT400": []
+  "STAT400": [
+    {
+      "title": "Homework assignment 1",
+      "course": "STAT400",
+      "dueDate": "2024-02-03T23:59:00",
+      "link": "https://www.gradescope.com/courses/720522",
+      "submitted": false
+    }
+  ]
 };

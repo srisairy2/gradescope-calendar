8d23fd9fa4bc0e10d26c0858966b9fa29012289f
planit <planit@tanjeffreyz.github.io>
2024-02-15 18:38:16 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -36,6 +36,13 @@ assignments = {
       "dueDate": "2024-02-28T23:59:00",
       "link": "https://www.gradescope.com/courses/723511",
       "submitted": false
+    },
+    {
+      "title": "Practice Midterm 1 - Summer 2023",
+      "course": "CMSC 430",
+      "dueDate": "2024-05-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/723511",
+      "submitted": false
     }
   ],
   "STAT400": [

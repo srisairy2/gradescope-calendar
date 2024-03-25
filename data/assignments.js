6dc6f34b1d24e7f47876ce5fd4e245f480707621
planit <planit@tanjeffreyz.github.io>
2024-03-25 19:37:56 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -68,7 +68,7 @@ assignments = {
     {
       "title": "Assignment 4, Part 1",
       "course": "CMSC 430",
-      "dueDate": "2024-03-25T23:59:00",
+      "dueDate": "2024-03-27T23:59:00",
       "link": "https://www.gradescope.com/courses/723511",
       "submitted": false
     }
